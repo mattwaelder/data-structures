7f77327071ae17675412f2d3e4e7c8c1e0e96a07
Matt Waelder <mattwaelder@gmail.com>
2022-07-20 15:37:55 -0700

    first commit

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,57 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var queueLen = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //putting property at the end of storage
+    if (queueLen === 0) {
+      // console.log('push w/ len 0');
+      storage[0] = value;
+      // console.log(storage);
+    } else {
+      // console.log('push w/ len > 0');
+      storage[queueLen] = value;
+      // console.log(storage);
+    }
+    queueLen++;
+    // console.log('new queue length', queueLen);
   };
+
+
 
   someInstance.dequeue = function() {
+    //removing property from the beginning of storage
+    var completedQueue = storage[0];
+    delete storage[0];
+    // console.log('being removed', completedQueue);
+    // console.log('post removal', storage);
+    //readjusting all other properties in storage (moving up) ??
+    // Object.keys(storage).forEach(function(el) {
+    //   el--;
+    // });
+    for (var [key, value] of Object.entries(storage)) {
+      // console.log(key, value);
+      storage[key - 1] = value;
+      delete storage[queueLen - 1];
+      // console.log(key, value);
+    }
+
+    queueLen <= 1 ? queueLen = 0 : queueLen--;
+
+    // console.log('storage', storage);
+    // console.log('queueLen', queueLen);
+    return completedQueue;
   };
 
+
+
+
   someInstance.size = function() {
+    //reading / reporting current size of queue
+    return queueLen;
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,59 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var currLen = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //if there is storage
+    if (currLen > 0) {
+      // console.log('current len > 0 adding', value);
+      storage[currLen] = value;
+      // console.log(storage);
+
+      currLen++;
+      // console.log(currLen);
+    } else {
+    //if storage is empty
+      // console.log('current len 0 adding', value);
+      storage[0] = value;
+      // console.log(storage);
+
+      currLen++;
+      // console.log(currLen);
+    }
   };
 
   someInstance.pop = function() {
+    //temp storage for popped value
+    let popped = storage[currLen - 1];
+
+
+    // console.log(storage);
+    // console.log('currLen:', currLen);
+    // console.log('popped', popped);
+
+
+    //now safe to remove actual value at top
+    delete storage[currLen - 1];
+    //reduce length variable (but not less than 0)
+    // currLen <= 1 ? currLen = 0 : currLen--;
+    if (currLen <= 1) {
+      currLen = 0;
+    } else {
+      currLen --;
+    }
+    //return popped value;
+    return popped;
+    // return storage[currLen];
   };
 
   someInstance.size = function() {
+    return currLen;
   };
 
   return someInstance;
 };
+
+
+
