ae36348f2c5e607109e924a63af263954d738262
Matt Waelder <mattwaelder@gmail.com>
2022-07-21 21:32:08 -0700

    completion of linked list, tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,78 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.previousNode;
+  // list.count = 0;
 
   list.addToTail = function(value) {
+    //make a new node with passed in value
+    let newNode = new Node(value);
+    let currentNode = newNode;
+
+    // if linked list is empty
+    if (!list.head) {
+      //set both head and tail to the new node
+      list.head = currentNode;
+      list.tail = currentNode;
+
+      //set a varaible to keep track of the last node put in
+      previousNode = currentNode;
+
+    // if linked list is NOT empty
+    } else {
+      //set current node to be the new tail;
+      list.tail = currentNode;
+      //set the previous nodes.next value to be current node;
+      previousNode.next = currentNode;
+
+      /////
+      //i dont think previous node is a good name, here, since we're only setting it when we add a head... ?
+      /////
+    }
   };
+
 
   list.removeHead = function() {
+    //make head point to current heads .next
+    var previousHead = list.head;
+    list.head = list.head.next;
+    return previousHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, currentNode) {
+    currentNode = currentNode || list.head;
+    //while there is a next value
+    // var confirm = false;
+
+    // while (currentNode.next) {
+    // let head = list.head;
+    // let currentNode;
+    // let nextNode = currentNode.next;
+
+
+    if (currentNode.value === target) {
+      //should set previous EC to true, and output to true;
+      return true;
+    }
+
+    //recursively calling next item in linked list
+    if (currentNode.next) {
+      var output = list.contains(target, currentNode.next);
+    }
+
+    //if output gets to be true, return true;
+    if (output) {
+      return true;
+    //else return false;
+    } else {
+      return false;
+    }
+
+
+
+    //check to see if current node value matches target
+    //if current node value doesnt equal target
+    //check current nodes .next
   };
 
   return list;
@@ -19,7 +83,10 @@ var Node = function(value) {
   var node = {};
 
   node.value = value;
+  //points to next node
   node.next = null;
+
+  // list.addToTail(value);
 
   return node;
 };
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,79 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+// newTree = {value: value, children: [ {value: value, children: [{}, {}, {}]} ]
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create new tree
+  let newChild = new Tree(value);
+  //add the new tree into children array
+  this.children.push(newChild);
+  // console.log(this);
+  // console.log('POST CHILD ADD:', newChild);
 };
 
-treeMethods.contains = function(target) {
-};
+treeMethods.contains = function(target, currentNode) {
+  console.warn('/////////CONTAINS CALL//////////');
+  currentNode = currentNode || this;
+  console.log('PASSED IN: ', currentNode);
 
+  var result = false;
+
+  if (currentNode.value === target) {
+    // console.log(currentNode.value);
+    // console.log('HAD VALUE, RETURNING TRUE');
+    result = true;
+  }
+
+  // if (currentNode.children.length === 0) {
+  //   if (currentNode.value !== target) {
+  //     return;
+  //   }
+  // }
+  // // if (currentNode.children.length === 0 && currentNode.value !== target) {
+  // //   console.log('looking for', target);
+  // //   console.log(currentNode.children.length, currentNode.value);
+  // //   console.log('NO CHILDREN, WRONG VALUE, VERY BAD');
+  // //   return;
+  // // }
+
+  if (currentNode.children.length !== 0) {
+    console.log('RECURSIVE CALL');
+    for (let i = 0; i < currentNode.children.length; i++) {
+      var currentChild = currentNode.children[i];
+      var hasNode = this.contains(target, currentChild);
+
+      if (hasNode) {
+        result = true;
+      }
+    }
+  }
+
+
+  //   console.warn('ENDING FUNCTION CALL');
+  //   if (hasNode) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+
+
+  return result;
+
+
+  //if current nodes value != target &&
+  //if node has children
+  //recursively call children array
+};
 
 
 /*
