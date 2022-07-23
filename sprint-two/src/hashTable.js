15f53946b7d48bd638f08d29bb34a066fb9bde67
Matt Waelder <mattwaelder@gmail.com>
2022-07-22 21:20:09 -0700

    progress on hash table, hit the wall while implementing collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,30 +3,65 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
 //   limitedArray.set(3, 'hi');
 //   limitedArray.get(3); // returns 'hi'
 
+// { [], [], [[k,v]], [], [], [] }
+//    1   2     3      4
+
 
 HashTable.prototype.insert = function(k, v) {
-  //creating our hash index
+  // grab index and check if result is equal to an array, push in tuples to array
+  // else, create sub array, and push in value
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //making new tuple
+  let tuple = [k, v];
+  // console.log('our touple:', tuple);
+  // console.log('our index:', index);
+  var currIndex = this._storage.get(index);
+  // console.log('CURRENT INDEX', currIndex);
 
-  console.log('key:', k, 'value:', v, 'index:', index);
+  //////////////////////////////////////////////////////////////////
+  //collisions!!!! loops?
+  //when adding two different keys, but get same index
+  //if the index is filled, but the key(s) arent the same
+  //add new tuple somehow
 
-  this._storage.set(index, [k, v]);
+  //we will need cases for
+  //if the index is empty
+  //if the index has tuple in it w/ different key
+  //if the index has tuple in it with same key (COLLISION)
+  //                    collision
+  //    [    [ [val1, val1], [val2, val2] ]      ]
+  //////////////////////////////////////////////////////////////////
 
-  //k = steven, v = seagal, index = 3
-  //add [steven, seagal] to index 3 of limitedArray
+  //if there is nothing at current index
+  if (!currIndex) {
+    console.log('nothing at index, ADDING');
+    //adding tuple to index
+    this._storage.set(index, tuple);
+  } else {
+    //this will no longer suffice.......
+    this._storage.set(index, [k, v]);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  return this._storage.get(index)[1];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //when removing key, make sure to remove entire tuple
+  this._storage.set(index, []);
+
 };
 
 
